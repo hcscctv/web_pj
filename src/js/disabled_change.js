47080b4403d6c4eae31d8cb7cd8a06c68af6de54
hcscctv <19302010004@fudan.edu.cn>
2020-04-09 14:54:57 +0800

    test

---
+++ src/js/disabled_change.js
@@ -0,0 +1,9 @@
+function disabled_change_a() {
+    document.getElementById("title").disabled = false;
+    document.getElementById("desc").disabled = true;
+}
+
+function disabled_change_b() {
+    document.getElementById("title").disabled = true;
+    document.getElementById("desc").disabled = false;
+}
---
+++ src/js/upload.js
@@ -0,0 +1,18 @@
+document.getElementById("file0").onchange = function() {
+    var objUrl = getObjectURL(document.getElementById("file0").files[0]);
+    if (objUrl != "")
+        document.getElementById("img0").setAttribute("src", objUrl)
+    document.getElementById("button").innerHTML = "重新上传"
+};
+
+function getObjectURL(file) {
+    var url = null;
+    if (window.createObjectURL != undefined) {
+        url = window.createObjectURL(file);
+    } else if (window.URL != undefined) {
+        url = window.URL.createObjectURL(file);
+    } else if (window.webkitURL != undefined) {
+        url = window.webkitURL.createObjectURL(file);
+    }
+    return url;
+}
